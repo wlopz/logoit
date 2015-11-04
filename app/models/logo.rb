7a453b6a6b165d64c2aa7634d7f10439b6afde10
Albert Galvez <albertomgalvez@gmail.com>
2015-11-04 16:10:17 -0500

    chaning the path for images to see if heroku allows me to write to it's temp files

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,6 +1,6 @@
 class Logo < ActiveRecord::Base
 	  has_attached_file :image, 
 	  :styles => { large: "640x480>", medium: "320x240"},
-      :url => "/tmp/:attachment/:id/:style_:filename"
+      :path => "/tmp/:attachment/:id/:style_:filename"
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
