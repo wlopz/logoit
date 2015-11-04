4af5e5947fb05a67d3b70fbbc571fcf815ab3357
Albert Galvez <albertomgalvez@gmail.com>
2015-11-04 16:05:51 -0500

    another experimental fix

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,8 +1,6 @@
 class Logo < ActiveRecord::Base
 	  has_attached_file :image, 
 	  :styles => { large: "640x480>", medium: "320x240"},
-	  :storage => :s3, 
-      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-      :path => "logoit/:attachment/:style/:id.:extension"
+      :url => "/tmp/:attachment/:id/:style_:filename"
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
