a9615bd25d9c3991ec4ecd2a2ce14b9a0623eea4
Albert Galvez <albertomgalvez@gmail.com>
2015-11-04 16:02:10 -0500

    experimental fix for a heroku porblem with paper clip uploads

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,4 +1,8 @@
 class Logo < ActiveRecord::Base
-	  has_attached_file :image, styles: { large: "640x480>", medium: "320x240"}, default_url: "/images/:style/missing.png"
+	  has_attached_file :image, 
+	  :styles => { large: "640x480>", medium: "320x240"},
+	  :storage => :s3, 
+      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+      :path => "logoit/:attachment/:style/:id.:extension"
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
