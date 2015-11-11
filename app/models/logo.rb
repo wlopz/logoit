e4b023fe37e2ffb038509b0c90ebdc2c93bc2ed7
Wilfredo Lopez <jrlopez85@gmail.com>
2015-11-10 20:39:32 -0500

    New company info stylized

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -2,10 +2,10 @@ class Logo < ActiveRecord::Base
 	  has_attached_file :image, 
 	  :styles => { large: "640x480>", medium: "320x240"},
 	  :storage => :s3, 
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => "logoit2",
       :s3_credentials => { 
-      	:access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
-      	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
+      	:access_key_id => "AKIAILZYGVGH5DJJCTDQ", 
+      	:secret_access_key => "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" 
       }
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	  belongs_to :campaign
