94ab4776db3ee16677018a671939a89ef5cc1135
Wilfredo Lopez <jrlopez85@gmail.com>
2015-11-13 11:54:40 -0500

    Added bootstrap to landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   	def after_sign_in_path_for(user)
   		dashboard_index_path
