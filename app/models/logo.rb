db58beea689549661949176840be405ed8114961
Albert Galvez <albertomgalvez@gmail.com>
2015-11-14 17:08:25 -0500

    fixed front page, and other front end modifications

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -5,8 +5,10 @@ class Logo < ActiveRecord::Base
       # :bucket => ENV['S3_BUCKET_NAME'],	
       :bucket => 'logoit2',
       :s3_credentials => { 
-      	:access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
-      	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
+      	# :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+      	# :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
+      	:access_key_id => "AKIAJIYXPJN66YDD6LFA", 
+      	:secret_access_key => "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"
       }
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	  belongs_to :campaign
